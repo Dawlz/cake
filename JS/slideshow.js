3b137248f53d7480d7b07dcfac7a101e94d8d561
Code With Mercy <64808015+mercyharbo@users.noreply.github.com>
2021-03-13 03:47:13 +0100

    Add files via upload

---
+++ JS/slideshow.js
@@ -0,0 +1,28 @@
+function NavbarToggle() {
+  let navs = document.querySelectorAll(".Nav-items")
+
+  navs.forEach(nav => nav.classList.toggle("Nav-toggleShow"));
+}
+
+document.querySelector(".Nav-toggle").addEventListener("click", NavbarToggle);
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
